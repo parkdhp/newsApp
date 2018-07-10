066a03ced30c3a8249d16537ecb71566f7f965b7
Peter Park <parkdhp@gmail.com>
2018-07-10 19:25:51 -0400

    init

---
+++ client/src/components/newsApp.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+
+class NewsApp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+
+  ComponentDidMount(){
+
+  }
+  
+  render() {
+    return (
+      <div>your news</div>
+    )
+  }
+}
---
+++ client/src/index.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/styles.css';
+import NewsApp from './components/newsApp.jsx';
+
+ReactDOM.render(<NewsApp />, document.getElementById('app'));
